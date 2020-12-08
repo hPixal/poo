ec84ce22608e88ca83f58713e82a5a530ddfbebf
BuildTools <unconfigured@null.spigotmc.org>
2020-12-08 12:49:54 -0300

    Aplicacion corre

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,19 @@
-#include <SFML/Graphics.hpp>
+ 
+#include <iostream>
+#include "Game.hpp"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(600,1000), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	srand(static_cast<unsigned>(time(0)));
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	Game game;
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	while (game.getWindow().isOpen())
+	{
+		game.update();
+		game.render();
+	}
 
-    return 0;
+	//End of application
+	return 0;
 }
