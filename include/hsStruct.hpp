ce6790ecea183150219d0f00b11482ce280614f1
hPixal <nahuel.j.gareis@protonmail.com>
2021-02-25 21:22:20 -0300

    added screenshots and licence to files

--- include/hsStruct.hpp
+++ include/hsStruct.hpp
@@ -32,3 +32,19 @@ struct hsStruct{
 	int _points;
 };
 
+/*
+This file is part of Skyjump.
+
+    Skyjump is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Skyjump is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Skyjump.  If not, see <https://www.gnu.org/licenses/>.
+*/
--- include/moving_cloud_platform.hpp
+++ include/moving_cloud_platform.hpp
@@ -16,3 +16,20 @@ public:
     bool collideWith(const Entity & ent2) override; //Checks if the player is colliding with this->_spr (if it already had once, this function is going to return false)
     ~moving_cloud_platform() override;
 };
+
+/*
+This file is part of Skyjump.
+
+    Skyjump is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Skyjump is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Skyjump.  If not, see <https://www.gnu.org/licenses/>.
+*/
